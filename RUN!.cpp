8fd30cec53201857be359d088a9772e1a8d08f0e
toro6021 <44702417+toro6021@users.noreply.github.com>
2019-01-29 20:55:18 +0800

    Add files via upload

---
+++ RUN!.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+#include<conio.h>
+
+int main()
+{
+	char ch=getch();
+	while(ch!='p' && ch!='P')
+	{
+		system("#.exe");
+		ch=getch();
+	}
+	return 0;
+}
